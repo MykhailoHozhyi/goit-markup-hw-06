da4f05d173045edd579de1c6a20ffdd983468beb
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-08-30 15:59:00 +0300

    add js

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const orderBtn = document.querySelector('.hero-button');
+const backdropEl = document.querySelector('.backdrop');
+const modalCloseBtn = document.querySelector('.modal-close-btn');
+const modalCloseIcon = document.querySelector('.modal-close-icon');
+const modalForm = document.querySelector('.modal-form');
+
+const menuBtn = document.querySelector('.menu-btn');
+const mobileMenu = document.querySelector('.mob-menu');
+const mobileMenuCloseBtn = document.querySelector('.mob-menu-close-btn');
+
+orderBtn.addEventListener('click', onOrderBtnClick);
+backdropEl.addEventListener('click', onBackdropElClick);
+modalForm.addEventListener('submit', onModalFormSubmit);
+
+menuBtn.addEventListener('click', onMenuBtnClick);
+mobileMenuCloseBtn.addEventListener('click', onMobileMenuCloseBtnClick);
+
+function onOrderBtnClick() {
+  backdropEl.classList.add('is-open');
+  document.body.classList.add('no-scroll');
+}
+
+function onBackdropElClick(event) {
+  if (
+    event.target === event.currentTarget ||
+    event.target === modalCloseBtn ||
+    event.target === modalCloseIcon
+  ) {
+    backdropEl.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
+  }
+}
+
+function onModalFormSubmit(event) {
+  event.preventDefault();
+
+  modalForm.reset();
+}
+
+function onMenuBtnClick() {
+  mobileMenu.classList.add('is-open');
+  document.body.classList.add('no-scroll');
+}
+
+function onMobileMenuCloseBtnClick() {
+  mobileMenu.classList.remove('is-open');
+  document.body.classList.remove('no-scroll');
+}
